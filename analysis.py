c53682929cead91d98ffc821e15e21f2d1d72af8
heihei <>
2021-12-15 22:57:23 +0900

    複数条件で絞り込み処理追加

--- analysis.py
+++ analysis.py
@@ -18,3 +18,23 @@ print(data['Score_Home'].mean())
 
 # ホームチームのゴール数の合計を計算（Score_Homeより）
 print(data['Score_Home'].sum())
+
+# 川崎フロンターレがホームの場合
+print(data.set_index('Home').loc['Kawasaki-F'])
+
+# 川崎フロンターレがホームの試合のスタッツ
+print(data.set_index('Home').loc['Kawasaki-F'].mean())
+
+# 各チームのホーム側の1試合当たりの得点、失点
+# teamList = []
+# for team in data.set_index('Home').index:
+#     if team not in teamList:
+#         teamList.append(team)
+#     team_summary = {}
+#     for teams in range(len(teamList)):
+#         team_summary[teamList[teams]] = data.set_index('Home').loc[teamList[teams]].mean()
+#         print('team : ' + team)
+#         print(team_summary[teamList[teams]])
+
+# 2017年の川崎フロンターレのホームの1試合平均
+print(data.query('Home == "Kawasaki-F" & Year == 2017').mean())
